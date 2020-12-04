2d99555eeab27096cf18505be55e37dba136341b
Aidan Knuth <knuthao@mail.uc.edu>
2020-12-04 13:41:59 -0500

    adding project 3

---
+++ Project3/FinalProjectThree.py
@@ -0,0 +1,46 @@
+import re
+import json
+import requests
+
+count =1
+
+print('    Welcome to TDW Version 3.0!   ')
+print('I can provide the Time, Date, and Weather for any U.S. zipcode.')
+print('Enter your zipcode here:')
+while count>0:
+    zip= input()
+    if re.search("^[0-9]{5}(?:-[0-9]{4})?$", zip):
+        count=count-1
+    else:
+        print('This is not a valid ZIP code. Enter a valid zipcode:')
+
+tzapi = requests.get("http://www.zipcodeapi.com/rest/xW708eUARknS04iKfYzRl0q5bXbnDHVXd5XKtzsCjLvJp1V17hdKIxZAx2cj4zBS/info.json/%s/degrees" % zip)
+tzdata = tzapi.json()
+tz = tzdata['timezone']['timezone_identifier']
+
+tdapi = requests.get("https://api.ipgeolocation.io/timezone?apiKey=a2e669fce72a4083a49021d7a5125711&tz=%s" % tz)
+tddata = tdapi.json()
+time = tddata['time_12']
+datewt = tddata['date_time_txt']
+date = datewt[:-8]
+
+wapi = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=68216b2c61a58b2d3f3e1f481e601e78" % zip)
+wdata = wapi.json()
+weather = wdata['weather'][0]['description']
+
+recount = 1
+print('Now type "Time", "Date", or "Weather"!')
+dtb = input().lower()
+
+while recount>0:
+    if dtb == "time":
+        print(time)
+        recount=recount-1
+    elif dtb == "date":
+        print(date)
+        recount=recount-1
+    elif dtb == "weather":
+        print(weather)
+        recount=recount-1
+    else:
+        print('Unfortunately we do not provide that feature yet. Please enter "Time", "Date", or "Weather":')
---
+++ Project3/proj3script.py
@@ -0,0 +1,42 @@
+﻿from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
+curdate = date.today()
+showdate = curdate.strftime("%B %d, %Y")
+curtime = datetime.now()
+
+
+print('    Welcome to TDW Version 3.0!   ')
+print('I can provide the Time, Date, and Weather for any U.S. zipcode.')
+print('Enter your zipcode here:')
+zip= input()
+answer=1
+print('Now type "Time", "Date", or "Weather"!')
+dtb = input().lower()
+if dtb == 'time' or dtb == 'both':
+    print('Please select one of the available timezones. The options are "Eastern", "Central", "Mountain", and "Pacific" Standard Time.')
+    tz = input().lower()
+    if tz =="central":
+        curtime = datetime.now() - timedelta(hours=1)
+    elif tz =="mountain":
+        curtime = datetime.now() - timedelta(hours=2)
+    elif tz =="pacific":
+        curtime = datetime.now() - timedelta(hours=3)
+    else:
+        print('That is not a valid time zone. The default time zone (Eastern) has been used.')
+showtime = curtime.strftime("%I:%M")
+while answer > 0:
+    if dtb == 'time':
+        print("The current time is ", showtime, "!")
+        answer=answer-1
+    elif dtb == 'date':
+        print("The curent date is ", showdate, "!")
+        answer=answer-1
+    elif dtb == 'both':
+        print("The current time is ", showtime, " and the current date is ", showdate,"!")
+        answer=answer-1
+    else: 
+        print("Please enter a valid option. Check your Capitalization!")
+        dtb = input()
+print("Check back any time for the current date, time, or both! Now in any timezone!")
